e8519ce65c1f9144d8b0838f4ad9f2d8ae4233b4
Laura Leite <1458979@sga.pucminas.br>
2024-05-01 21:44:40 -0300

    Adicionada tabela produtos

--- src/Migrations/20240502003450_M8-AddTableProdutos.Designer.cs
+++ src/Migrations/20240502003450_M8-AddTableProdutos.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pharma.Models;
 
@@ -10,9 +11,11 @@ using Pharma.Models;
 namespace Pharma.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240502003450_M8-AddTableProdutos")]
+    partial class M8AddTableProdutos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.4");
---
+++ src/Models/Produtos.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pharma.Models
+{
+    [Table("Produtos")]
+    public class Produtos
+    {
+        [Key]
+        public int IdProduto { get; set; }
+
+
+        [ForeignKey("Usuario")]
+        public int IdUsuario { get; set; }
+        public virtual Usuario Usuario { get; set; }
+
+
+        [ForeignKey("Categorias")]
+        public int IdCategoria { get; set; }
+        public virtual Categoria Categoria   { get; set; }
+
+
+        [ForeignKey("Localizacoes")]
+        public int IdLocalizacao  { get; set; }
+        public virtual Localizacao Localizacao { get; set; }
+
+
+        public string NomeProduto { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DtCadastro { get; set; }
+    }
+}
