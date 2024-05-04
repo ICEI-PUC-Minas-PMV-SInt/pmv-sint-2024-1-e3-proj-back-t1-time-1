d9265f3d227c03f075315c2ee912192c59562089
alessandraMurat <alessandra.murat@outlook.com>
2024-05-04 18:00:17 -0300

    Criação de operação de edição de categorias

--- src/Controllers/CategoriasController.cs
+++ src/Controllers/CategoriasController.cs
@@ -18,18 +18,20 @@ namespace Pharma.Controllers
         {
             IEnumerable<Categoria> dados;
 
-            Debug.WriteLine(User.IsInRole("Admin"), "testeee");
 
             if (User.IsInRole("Admin"))
             {
-                 dados = await _context.Categorias.ToListAsync();
+                 dados = await _context.Categorias.Include(c => c.Usuario).ToListAsync();
+     
 
             } else
             {
                 var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
                 dados = await _context.Categorias
-            .Where(c => c.UsuarioId == int.Parse(userId))
-            .ToListAsync();
+                    .Include(c => c.Usuario)
+                    .Where(c => c.UsuarioId == int.Parse(userId))
+                    .ToListAsync();
+          
             }
            
             return View(dados);
@@ -56,12 +58,46 @@ namespace Pharma.Controllers
                 // Defina o UsuarioId da categoria com o ID do usuário logado
                 categoria.UsuarioId = int.Parse(userId);
                 _context.Categorias.Add(categoria);
-
-                Debug.WriteLine(userId, categoria, "testeeeeeeeee");
                 await _context.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
             return View(categoria);
+        }
+
+        public async Task<IActionResult> Edit(int? id) {
+     
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var dados = await _context.Categorias.FindAsync(id);
+        
+            
+            if (dados == null)
+            {
+                return NotFound();
+            }
+            return View(dados);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Categoria categoria)
+        {
+
+            if (id != categoria.Id_categoria)
+            {
+                return NotFound();
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                _context.Categorias.Update(categoria);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View();
         }
     }
 }
