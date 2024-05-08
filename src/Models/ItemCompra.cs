e47ea6e857e225d9c9382a27fcde78b0892c560e
celsonunessoares <celso.the@gmail.com>
2024-05-07 22:04:36 -0300

    Adicionado a Tabela Item Compra

---
+++ src/Models/ItemCompra.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pharma.Models
+{
+    [Table("ItemCompra")]
+    public class ItemCompra
+    {
+        [Key]
+        public int IdItemCompra { get; set; }
+
+        [ForeignKey("PedidoCliente")]
+        public int IdCompra { get; set; }
+        public virtual PedidoCliente PedidoCliente { get; set; }
+
+        //[ForeignKey("Produtos")]
+        //public int IdProduto { get; set; }
+        //public virtual Produtos Produtos { get; set; }
+
+        public int QtProduto { get; set; }
+        public decimal VlTotalItem { get; set; }
+        public decimal VlUnitarioItem { get; set; }
+    }
+
+}
+
