a332117dc63836c18a9b9194dd4405be66890a7a
alessandraMurat <alessandra.murat@outlook.com>
2024-06-01 23:00:13 -0300

    Modificação de categoria

--- src/Controllers/CategoriasController.cs
+++ src/Controllers/CategoriasController.cs
@@ -1,163 +1,160 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Pharma.Models;
+using System.Diagnostics;
+using System.Security.Claims;
 
 namespace Pharma.Controllers
 {
     public class CategoriasController : Controller
     {
+        //Contexto do banco de dados 
         private readonly AppDbContext _context;
-
         public CategoriasController(AppDbContext context)
         {
             _context = context;
         }
 
-        // GET: Categorias
         public async Task<IActionResult> Index()
         {
-            var appDbContext = _context.Categoria.Include(c => c.Usuarios);
-            return View(await appDbContext.ToListAsync());
+            IEnumerable<Categoria> dados;
+
+
+            if (User.IsInRole("Admin"))
+            {
+                dados = await _context.Categoria.Include(c => c.Usuarios).ToListAsync();
+
+
+            }
+            else
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                dados = await _context.Categoria
+                    .Include(c => c.Usuarios)
+                    .Where(c => c.UsuarioId == int.Parse(userId))
+                    .ToListAsync();
+
+            }
+
+            return View(dados);
         }
 
-        // GET: Categorias/Details/5
+        //Método Get
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        //Método Post
+        [HttpPost]
+        public async Task<IActionResult> Create(Categoria categoria)
+        {
+
+
+
+            if (ModelState.IsValid)
+            {
+
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                // Defina o UsuarioId da categoria com o ID do usuário logado
+                categoria.UsuarioId = int.Parse(userId);
+                _context.Categoria.Add(categoria);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(categoria);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var dados = await _context.Categoria.FindAsync(id);
+
+
+            if (dados == null)
+            {
+                return NotFound();
+            }
+            return View(dados);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Categoria categoria)
+        {
+
+            if (id != categoria.Id)
+            {
+                return NotFound();
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                _context.Categoria.Update(categoria);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
-
-            var categoria = await _context.Categoria
-                .Include(c => c.Usuarios)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (categoria == null)
+            var dados = await _context.Categoria.Include(c => c.Usuarios)
+        .FirstAsync(c => c.Id == id);
+            if (dados == null)
             {
                 return NotFound();
             }
-
-            return View(categoria);
+            ViewBag.AcessoUsuario = dados.Usuarios.AcessoUsuario;
+            return View(dados);
         }
 
-        // GET: Categorias/Create
-        public IActionResult Create()
-        {
-            ViewData["UsuarioId"] = new SelectList(_context.Usuario, "Id", "AcessoUsuario");
-            return View();
-        }
-
-        // POST: Categorias/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nome,UsuarioId")] Categoria categoria)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(categoria);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["UsuarioId"] = new SelectList(_context.Usuario, "Id", "AcessoUsuario", categoria.UsuarioId);
-            return View(categoria);
-        }
-
-        // GET: Categorias/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var categoria = await _context.Categoria.FindAsync(id);
-            if (categoria == null)
-            {
-                return NotFound();
-            }
-            ViewData["UsuarioId"] = new SelectList(_context.Usuario, "Id", "AcessoUsuario", categoria.UsuarioId);
-            return View(categoria);
-        }
-
-        // POST: Categorias/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,UsuarioId")] Categoria categoria)
-        {
-            if (id != categoria.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(categoria);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!CategoriaExists(categoria.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["UsuarioId"] = new SelectList(_context.Usuario, "Id", "AcessoUsuario", categoria.UsuarioId);
-            return View(categoria);
-        }
-
-        // GET: Categorias/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
             {
                 return NotFound();
             }
+            var dados = await _context.Categoria.Include(c => c.Usuarios)
+        .FirstAsync(c => c.Id == id);
+            if (dados == null)
+            {   
+                return NotFound();
+            }
+            ViewBag.AcessoUsuario = dados.Usuarios.AcessoUsuario;
+            return View(dados);
+        }
 
-            var categoria = await _context.Categoria
-                .Include(c => c.Usuarios)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (categoria == null)
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var dados = await _context.Categoria.Include(c => c.Usuarios)
+        .FirstAsync(c => c.Id == id);
+            if (dados == null)
             {
                 return NotFound();
             }
 
-            return View(categoria);
-        }
-
-        // POST: Categorias/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var categoria = await _context.Categoria.FindAsync(id);
-            if (categoria != null)
-            {
-                _context.Categoria.Remove(categoria);
-            }
-
+            _context.Categoria.Remove(dados);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
 
-        private bool CategoriaExists(int id)
-        {
-            return _context.Categoria.Any(e => e.Id == id);
+
+            return RedirectToAction("Index");
         }
     }
 }
